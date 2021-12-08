0fd67ea3c571f3adc384c0119948fdbdd53c0350
professorK <jeffpk@gmail.com>
2021-12-08 17:41:23 -0500

    Iniial setup

---
+++ RawInputLight/RawInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+using Windows.Win32;
+using Windows.Win32.Storage.FileSystem;
+
+namespace RawInputLight
+{
+    public  class RawInput
+    {
+        private void Foo()
+        {
+            using SafeHandle handle = PInvoke.CreateFile("text.txt", 
+                FILE_ACCESS_FLAGS.FILE_ADD_FILE, 
+                FILE_SHARE_FLAGS.FILE_SHARE_WRITE, 
+                null,
+                FILE_CREATE_FLAGS.CREATE_ALWAYS, 
+                FILE_FLAGS_AND_ATTRIBUTES.FILE_ATTRIBUTE_ARCHIVE, 
+                CloseHandleSafeHandle.Null);
+        }
+    }
+}
