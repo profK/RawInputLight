99c8d97bd8983d605fbd615e4b247fdcd860f5e6
professorK <jeffpk@gmail.com>
2024-02-16 15:36:13 -0500

    Set up conditional compilation for the non Keybioard or mouse case, which was blowing up on test machine, this is a temporary solution while the failure is debugged. To reactivate controller reading set CONTROLLERS_HID on line 20 of NativeAPI.cs to true

--- RawInputLight/obj/Debug/net6.0/RawInputLight.AssemblyInfo.cs
+++ RawInputLight/obj/Debug/net6.0/RawInputLight.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RawInputLight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ab517dd4be99f9efa76f24344e69b2fcb236a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbea865cc10c07340fb644c5ae26a12345ed08b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RawInputLight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RawInputLight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
